b1b49e4e30c62a6fdbc99649018fd5984cf89d91
Sicong Luo <lclfy@icloud.com>
2018-02-12 11:37:55 +0800

    Initial commit

---
+++ TimeTableAutoCompleteTool/Main.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TimeTableAutoCompleteTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.command_rTb = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.importTimeTable_Btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.filePath_lbl = new System.Windows.Forms.Label();
+            this.start_Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(36, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.将客调命令复制于此";
+            // 
+            // command_rTb
+            // 
+            this.command_rTb.Location = new System.Drawing.Point(40, 61);
+            this.command_rTb.Name = "command_rTb";
+            this.command_rTb.Size = new System.Drawing.Size(414, 245);
+            this.command_rTb.TabIndex = 1;
+            this.command_rTb.Text = "";
+            this.command_rTb.TextChanged += new System.EventHandler(this.command_rTb_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(36, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2.选择时刻表文件";
+            // 
+            // importTimeTable_Btn
+            // 
+            this.importTimeTable_Btn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.importTimeTable_Btn.Location = new System.Drawing.Point(359, 326);
+            this.importTimeTable_Btn.Name = "importTimeTable_Btn";
+            this.importTimeTable_Btn.Size = new System.Drawing.Size(95, 32);
+            this.importTimeTable_Btn.TabIndex = 3;
+            this.importTimeTable_Btn.Text = "导入时刻表";
+            this.importTimeTable_Btn.UseVisualStyleBackColor = true;
+            this.importTimeTable_Btn.Click += new System.EventHandler(this.importTimeTable_Btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(36, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "已选择：";
+            // 
+            // filePath_lbl
+            // 
+            this.filePath_lbl.AutoSize = true;
+            this.filePath_lbl.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.filePath_lbl.Location = new System.Drawing.Point(105, 366);
+            this.filePath_lbl.Name = "filePath_lbl";
+            this.filePath_lbl.Size = new System.Drawing.Size(0, 17);
+            this.filePath_lbl.TabIndex = 5;
+            // 
+            // start_Btn
+            // 
+            this.start_Btn.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.start_Btn.Location = new System.Drawing.Point(40, 403);
+            this.start_Btn.Name = "start_Btn";
+            this.start_Btn.Size = new System.Drawing.Size(414, 65);
+            this.start_Btn.TabIndex = 6;
+            this.start_Btn.Text = "处理时刻表";
+            this.start_Btn.UseVisualStyleBackColor = true;
+            this.start_Btn.Click += new System.EventHandler(this.start_Btn_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 489);
+            this.Controls.Add(this.start_Btn);
+            this.Controls.Add(this.filePath_lbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.importTimeTable_Btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.command_rTb);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox command_rTb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button importTimeTable_Btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label filePath_lbl;
+        private System.Windows.Forms.Button start_Btn;
+    }
+}
+
---
+++ TimeTableAutoCompleteTool/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableAutoCompleteTool
+{
+    public partial class Main : Form
+    {
+        private Boolean hasText = false;
+        private Boolean hasFilePath = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            start_Btn.Enabled = false;
+        }
+
+        private void command_rTb_TextChanged(object sender, EventArgs e)
+        {
+            if(command_rTb.Text.Length != 0)
+            {
+                hasText = true;
+                startBtnCheck();
+            }
+            else
+            {
+                hasText = false;
+                startBtnCheck();
+            }
+        }
+
+        private void importTimeTable_Btn_Click(object sender, EventArgs e)
+        {
+            SelectPath();
+        }
+
+        private void SelectPath()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();   //显示选择文件对话框 
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Excel files (*.xls)|*.xls";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.filePath_lbl.Text = openFileDialog1.FileName;     //显示文件路径 
+                hasFilePath = true;
+                startBtnCheck();
+            }
+        }
+
+        private void startBtnCheck()
+        {
+            if(hasFilePath && hasText)
+            {
+                start_Btn.Enabled = true;
+            }
+            else
+            {
+                start_Btn.Enabled = false;
+            }
+        }
+
+        private void start_Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateTimeTable()
+        {
+
+        }
+    }
+}
