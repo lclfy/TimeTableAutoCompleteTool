16aa8db68bd720ff6b021124b253eb68e75530a1
Sicong Luo <lclfy@icloud.com>
2018-02-12 13:10:42 +0800

    0212-Build1
    数据处理完成

---
+++ TimeTableAutoCompleteTool/CommandModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTableAutoCompleteTool
+{
+    class CommandModel
+    {//车次号，停运状态
+        public string trainNumber { get; set; }
+        public Boolean streamStatus { get; set; }  
+    }
+}
--- TimeTableAutoCompleteTool/Main.cs
+++ TimeTableAutoCompleteTool/Main.cs
@@ -74,12 +74,95 @@ namespace TimeTableAutoCompleteTool
 
         private void start_Btn_Click(object sender, EventArgs e)
         {
+            analyseCommand();
+        }
 
+        private void analyseCommand()
+        {   //分析客调命令
+            String[] AllCommand = command_rTb.Text.ToString().Split('。');
+            List<CommandModel> AllModels = new List<CommandModel>();
+            for(int i = 0; i < AllCommand.Length; i++)
+            {
+                //若有英文冒号 则分割英文冒号，若有中文冒号，则分割中文冒号
+                String[] command;
+                String[] AllTrainNumberInOneRaw;
+                Boolean streamStatus = true;
+                if (AllCommand[i].Contains("：") &&
+                    !AllCommand[i].Contains(":"))
+                {
+                   command = AllCommand[i].Split('：');
+                    if (command.Length == 2 ||
+                        command.Length == 3)
+                    {
+                        //把车次单独分离-去中文-去横杠
+                        AllTrainNumberInOneRaw = System.Text.RegularExpressions.Regex.Replace(command[1], @"[\u4e00-\u9fa5]", "").Split('-');
+                        if(command.Length == 3)
+                            //标注停运状态
+                        {
+                            streamStatus = !command[2].Contains("停");
+                        }
+                        for(int k = 0; k < AllTrainNumberInOneRaw.Length; k++)
+                        {
+                            //处理单程双车次车辆
+                            if (AllTrainNumberInOneRaw[k].Contains("/"))
+                            {
+                                String[] trainWithDoubleNumber = AllTrainNumberInOneRaw[k].Split('/');
+                                //先添加第一个车次
+                                CommandModel m1 = new CommandModel();
+                                m1.trainNumber = trainWithDoubleNumber[0];
+                                m1.streamStatus = streamStatus;
+                                AllModels.Add(m1);
+
+                                Char[] firstTrainWord = trainWithDoubleNumber[0].ToCharArray();
+                                String secondTrainWord = "";
+                                for (int q = 0; q < firstTrainWord.Length; q++)
+                                {
+                                    if (q != firstTrainWord.Length - trainWithDoubleNumber[1].Length)
+                                    {
+                                        secondTrainWord = secondTrainWord + firstTrainWord[q];
+                                    }
+                                    else
+                                    {
+                                        secondTrainWord = secondTrainWord + trainWithDoubleNumber[1];
+                                        //添加第二个车次
+                                        CommandModel m2 = new CommandModel();
+                                        m2.trainNumber = secondTrainWord;
+                                        m2.streamStatus = streamStatus;
+                                        AllModels.Add(m2);
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                CommandModel model = new CommandModel();
+                                model.trainNumber = AllTrainNumberInOneRaw[k];
+                                model.streamStatus = streamStatus;
+                                AllModels.Add(model);
+                            }
+                        }
+   
+                    }
+
+                }else if(!AllCommand[i].Contains("：") &&
+                    AllCommand[i].Contains(":"))
+                {
+                    command = AllCommand[i].Split(':');
+
+                }
+                else if(AllCommand[i].Contains("：") &&
+                    AllCommand[i].Contains(":"))
+                {
+
+                }
+
+                
+            }
         }
 
         private void updateTimeTable()
         {
-
+      
         }
     }
 }
