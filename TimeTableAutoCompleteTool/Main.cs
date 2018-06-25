d427aab2dd9531d106b9547c9f900e161480172b
Sicong Luo <lclfy@icloud.com>
2018-06-25 20:54:15 +0800

    0624 B35

--- TimeTableAutoCompleteTool/Main.cs
+++ TimeTableAutoCompleteTool/Main.cs
@@ -35,13 +35,12 @@ namespace TimeTableAutoCompleteTool
         public int modeSelect;
         string upStations = "京广-（新乡东 安阳东 鹤壁东 邯郸东 石家庄 保定东 定州东 正定机场 邢台东 高碑店东 涿州东 北京西）石地区-（太原南 定州东 阳泉北 石家庄东 藁城南 辛集南 衡水北 景州 德州东 平原东 禹城东 齐河）京沪北-（北京南 廊坊 天津西 天津 天津南 沧州西 德州东 泰安 曲阜东 滕州东 枣庄）徐兰-（ 开封北 兰考南 商丘 永城北 砀山南 萧县北 徐州东）京沪南-（ 宿州东 蚌埠南 定远 滁州 南京南 南京 镇江南 丹阳北 常州北 无锡东 苏州 苏州北 昆山南 上海 上海虹桥）胶济-（济南西 威海 荣成 胶州北 高密 潍坊 昌乐 青州市 淄博 周村东 章丘 济南东 烟台 青岛北 青岛） 城际-（宋城路）  京东北-（ 辽阳 铁岭西 开原西 昌图西 四平东 公主岭南 长春西 德惠西 扶余北 双城北 哈尔滨西 秦皇岛 沈阳北 沈阳 承德南 承德 怀柔南 朝阳 大连北 长春 哈尔滨西 ） 郑东南-（ 合肥南 肥东 巢北 黄庵 全椒 江浦 黄山北 金华南 宁波 杭州东 温州南 义乌 松江南 金山北 嘉善南 嘉兴南 桐乡 海宁西 余杭 ） ";
         string downStations = "郑州 郑州西 京广-（ 许昌东 漯河西 驻马店西 信阳东 明港东 孝感北 武汉 汉口 咸宁北 赤壁北 岳阳东 汨罗东 长沙南 株洲西 衡山西 衡阳东 耒阳西 郴州西 韶关 英德西 清远 广州北 深圳北 福田 深圳北 广州南 庆盛 虎门 光明城 西九龙 珠海）城际-（ 新郑机场 焦作）徐兰-（ 巩义南 洛阳龙门 三门峡西 灵宝西 华山北 渭南北 临潼东 西安北 汉中 宝鸡南 天水南 秦安 通渭 定西北 榆中 兰州西）西南-（ 成都东 重庆西 重庆北 贵阳北 昆明南 南宁东 怀化南 湘潭北 韶山南 芷江 新晃西 娄底南 桂林 玉溪 宜昌东 恩施 襄阳北 汉川 天门南 仙桃西 潜江 荆州 枝江北）东南-（ 黄冈东 萍乡北 新余北 宜春东 鹰潭北 南昌西 九江  赣州西 厦门北 潮汕 漳州 惠州南）郑万-（长葛北 禹州东 郏县 平顶山西 方城 邓州东 南阳卧龙 襄阳东津 南漳 保康县 神农架 兴山 巴东北 巫山 奉节 云阳 万州北） 郑合-（许昌北 鄢陵南 扶沟南 西华 周口东 淮阳 沈丘北 界首南 临泉 阜阳西）";
-        string build = "build 34 - v180604";
-        string readMe = "build34更新内容:\n" +
-            "班计划创建时，修复了进站换向列车（G847/50等）的上/下行判断错误";
+        string build = "build 35 - v180624";
+        string readMe = "build35更新内容:\n" +
+            "Bug Fixes";
 
         public Main()
         {
-
             InitializeComponent();
         }
 
@@ -172,6 +171,7 @@ namespace TimeTableAutoCompleteTool
         {   //分析客调命令
             //删除不需要的标点符号-字符
             int addedTrainCount = 0;
+            
             try
             {
                 string wrongNumber = "";
@@ -182,10 +182,16 @@ namespace TimeTableAutoCompleteTool
                 for (int i = 0; i < AllCommand.Length; i++)
                 {
                     if(AllCommand[i].Contains("站")&&
-                        AllCommand[i].Contains("00")&&
-                        AllCommand[i].Contains("开")&&
-                        !AllCommand[i].Contains("00：")&&
-                        !AllCommand[i].Contains("：00") )
+                        AllCommand[i].Contains("开")&&(
+                        AllCommand[i].Contains("001") ||
+                        AllCommand[i].Contains("002") ||
+                        AllCommand[i].Contains("003") ||
+                        AllCommand[i].Contains("004") ||
+                        AllCommand[i].Contains("005") ||
+                        AllCommand[i].Contains("006") ||
+                        AllCommand[i].Contains("007") ||
+                        AllCommand[i].Contains("008") ||
+                        AllCommand[i].Contains("009")))
                     {//加开车次，单独存储
                         string addedCommand = AllCommand[i];
                         if (addedCommand.Contains("月") && addedCommand.Contains("日"))
@@ -529,13 +535,16 @@ namespace TimeTableAutoCompleteTool
                 }
                 outputTB.Text = "共" + AllModels.Count.ToString() + "趟" + "\r\n" + commands;
                 commandModel = AllModels;
-            }catch (Exception e)
+                
+            }
+            catch (Exception e)
             {
                 MessageBox.Show("出现错误："+e.ToString().Split('。')[0], "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-          
+            
 
-        }
+
+            }
 
         public bool IsTrainNumber(string input)
         {//判断是否是符合规范的车次 若不符合 则给予识别错误提示
@@ -776,7 +785,9 @@ namespace TimeTableAutoCompleteTool
                         }
                         else
                         {
-                            if (int.Parse(model.trainNumber.ToCharArray()[model.trainNumber.ToCharArray().Length - 1].ToString()) %2 == 0)
+                            int outNum = 0;
+                            int.TryParse(model.trainNumber.ToCharArray()[model.trainNumber.ToCharArray().Length - 1].ToString(), out outNum);
+                            if (outNum %2 == 0)
                             {//上行
                                 model.upOrDown = 0;
                             }
@@ -999,7 +1010,7 @@ namespace TimeTableAutoCompleteTool
                     }
                     int hour = -1;
                     int.TryParse(DateTime.Now.ToString("HH"), out hour);
-                    if (hour >= 0 && hour <= 6)
+                    if (hour >= 0 && hour <= 16)
                     {
                         title = DateTime.Now.ToString("yyyy年MM月dd日-") + title;
                     }
@@ -1125,10 +1136,84 @@ namespace TimeTableAutoCompleteTool
                                         }
                                         if (!ContainsTrainNumber)
                                         {
-                                            if (commandText.Contains(row.GetCell(j).ToString().Trim()))
+                                            string trainNum = row.GetCell(j).ToString().Trim();
+                                            //单车号
+                                            bool gotIt = false;
+                                            if (commandText.Contains(trainNum))
                                             {
-                                                checkedText = checkedText + " " + row.GetCell(j).ToString().Trim();
-                                                row.GetCell(j).SetCellValue("(核对)" + row.GetCell(j).ToString().Trim());
+                                                checkedText = checkedText + " " + trainNum;
+                                                row.GetCell(j).SetCellValue("(核对)" + trainNum);
+                                                gotIt = true;
+                                            }
+                                            //双车号
+                                            if (!gotIt)
+                                            {
+                                                string splitedNumber = "";
+                                                int originalTrainNumber = 0;
+                                                string trainType = "";
+                                                string targetString = "";
+                                                if (trainNum.Contains("G"))
+                                                {
+                                                    trainType = "G";
+                                                }
+                                                else if (trainNum.Contains("D"))
+                                                {
+                                                    trainType = "D";
+                                                }
+                                                else if (trainNum.Contains("C"))
+                                                {
+                                                    trainType = "C";
+                                                }
+
+                                                foreach (char item in trainNum)
+                                                {
+                                                    if (item >= 48 && item <= 58)
+                                                    {
+                                                        splitedNumber += item;
+                                                    }
+                                                }
+                                                int.TryParse(splitedNumber, out originalTrainNumber);
+                                                string targetTrainNum = "";
+                                                if (originalTrainNumber != 0)
+                                                {
+                                                    string compared = "";
+                                                    bool hasGotIt = false;
+                                                    for (int ij = 0; ij < 4; ij++)
+                                                    {//+1 -1 +3 -3分别试一遍(试该车次的第二个车号)
+                                                        if (hasGotIt)
+                                                        {
+                                                            break;
+                                                        }
+                                                        switch (ij)
+                                                        {
+                                                            case 0:
+                                                                targetTrainNum = trainType + (originalTrainNumber + 1).ToString() + "/";
+                                                                break;
+                                                            case 1:
+                                                                targetTrainNum = trainType + (originalTrainNumber - 1).ToString() + "/";
+                                                                break;
+                                                            case 2:
+                                                                targetTrainNum = trainType + (originalTrainNumber + 3).ToString() + "/";
+                                                                break;
+                                                            case 3:
+                                                                targetTrainNum = trainType + (originalTrainNumber - 3).ToString() + "/";
+                                                                break;
+                                                        }
+                                                        for (int index = 0; index < trainNum.Length; index++)
+                                                        {
+                                                            if (trainNum[index] != targetTrainNum[index])
+                                                            {
+                                                                targetTrainNum = targetTrainNum + trainNum[index];
+                                                            }
+                                                        }
+                                                        if (commandText.Contains(targetTrainNum))
+                                                        {
+                                                            checkedText = checkedText + " " + trainNum;
+                                                            row.GetCell(j).SetCellValue("(核对)" + trainNum);
+                                                            hasGotIt = true;
+                                                        }
+                                                    }
+                                                }
                                             }
                                             row.GetCell(j).SetCellValue("×" + row.GetCell(j).ToString().Trim());
                                             row.GetCell(j).CellStyle = stoppedTrainStyle;
@@ -1169,7 +1254,6 @@ namespace TimeTableAutoCompleteTool
                         {
 
                         }
-
                     }
                     try
                     {
@@ -1241,7 +1325,7 @@ namespace TimeTableAutoCompleteTool
             else if (radioButton3.Checked)
             {
                 modeSelect = 3;
-                startPath = "时刻表";
+                startPath = "动车所时刻表";
                 secondStepText_lbl.Text = "2.选择动车所时刻表";
                 start_Btn.Text = "补全车辆编组";
                 ExcelFile = new List<string>();
@@ -1599,34 +1683,20 @@ namespace TimeTableAutoCompleteTool
                         _ds.id = counter;
                         _ds.streamStatus = commandModel[j].streamStatus;
                         _ds.trainType = commandModel[j].trainType;
-                        if (commandModel[j].trainNumber.Contains("850"))
-                        {
-                            int kj = 0;
-                        }
                         if(commandModel[j].upOrDown != -1)
                         {
                             _ds.upOrDown = commandModel[j].upOrDown;
                         }
                         else
                         {
-                            if(upStations.Contains(dailyScheduleModel[i].startStation)&&
-                                upStations.Contains(dailyScheduleModel[i].stopStation))
-                            {//如果从上来-调头从上回去，则进站时为下行
-                                _ds.upOrDown = 1;
-                            }
-                            else if(downStations.Contains(dailyScheduleModel[i].startStation) &&
+                            if (upStations.Contains(dailyScheduleModel[i].startStation) ||
                                 downStations.Contains(dailyScheduleModel[i].stopStation))
-                            {//与上面情况相反
-                                _ds.upOrDown = 0;
-                            }
-                            else if (upStations.Contains(dailyScheduleModel[i].startStation) ||
-                                downStations.Contains(dailyScheduleModel[i].stopStation))
-                            {//普通下行车
+                            {//下行车
                                 _ds.upOrDown = 1;
                             }
                             else if (downStations.Contains(dailyScheduleModel[i].startStation) ||
                                 upStations.Contains(dailyScheduleModel[i].stopStation))
-                            {//普通上行车
+                            {//上行车
                                 _ds.upOrDown = 0;
                             }
                         }
