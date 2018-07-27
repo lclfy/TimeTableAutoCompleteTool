789860380b6f29c08c3835cf7d29b2099ad9ab5d
Sicong Luo <lclfy@icloud.com>
2018-07-27 17:03:09 +0800

    0727
    Bug fixes

--- TimeTableAutoCompleteTool/Main.cs
+++ TimeTableAutoCompleteTool/Main.cs
@@ -38,9 +38,9 @@ namespace TimeTableAutoCompleteTool
         public int modeSelect;
         string upStations = "京广-（新乡东 安阳东 鹤壁东 邯郸东 石家庄 保定东 定州东 正定机场 邢台东 高碑店东 涿州东 北京西）石地区-（太原南 定州东 阳泉北 石家庄东 藁城南 辛集南 衡水北 景州 德州东 平原东 禹城东 齐河）京沪北-（北京南 廊坊 天津西 天津 天津南 沧州西 德州东 泰安 曲阜东 滕州东 枣庄）徐兰-（ 开封北 兰考南 商丘 永城北 砀山南 萧县北 徐州东）京沪南-（ 宿州东 蚌埠南 定远 滁州 南京南 南京 镇江南 丹阳北 常州北 无锡东 苏州 苏州北 昆山南 上海 上海虹桥）胶济-（济南西 威海 荣成 胶州北 高密 潍坊 昌乐 青州市 淄博 周村东 章丘 济南东 烟台 青岛北 青岛） 城际-（宋城路）  京东北-（ 辽阳 铁岭西 开原西 昌图西 四平东 公主岭南 长春西 德惠西 扶余北 双城北 哈尔滨西 秦皇岛 沈阳北 沈阳 承德南 承德 怀柔南 朝阳 大连北 长春 哈尔滨西 ） 郑东南-（ 合肥南 肥东 巢北 黄庵 全椒 江浦 黄山北 金华南 宁波 杭州东 温州南 义乌 松江南 金山北 嘉善南 嘉兴南 桐乡 海宁西 余杭 ） ";
         string downStations = "郑州 郑州西 京广-（ 许昌东 漯河西 驻马店西 信阳东 明港东 孝感北 武汉 汉口 咸宁北 赤壁北 岳阳东 汨罗东 长沙南 株洲西 衡山西 衡阳东 耒阳西 郴州西 韶关 英德西 清远 广州北 深圳北 福田 深圳北 广州南 庆盛 虎门 光明城 西九龙 珠海）城际-（ 新郑机场 焦作）徐兰-（ 巩义南 洛阳龙门 三门峡西 灵宝西 华山北 渭南北 临潼东 西安北 汉中 宝鸡南 天水南 秦安 通渭 定西北 榆中 兰州西）西南-（ 成都东 重庆西 重庆北 贵阳北 昆明南 南宁东 怀化南 湘潭北 韶山南 芷江 新晃西 娄底南 桂林 玉溪 宜昌东 恩施 襄阳北 汉川 天门南 仙桃西 潜江 荆州 枝江北 湛江西）东南-（ 黄冈东 萍乡北 新余北 宜春东 鹰潭北 南昌西 九江  赣州西 厦门北 潮汕 漳州 惠州南）郑万-（长葛北 禹州东 郏县 平顶山西 方城 邓州东 南阳卧龙 襄阳东津 南漳 保康县 神农架 兴山 巴东北 巫山 奉节 云阳 万州北） 郑合-（许昌北 鄢陵南 扶沟南 西华 周口东 淮阳 沈丘北 界首南 临泉 阜阳西）";
-        string build = "build 38 - v180726";
-        string readMe = "build38更新内容:\n" +
-            "现在可以判断旅客列车有无出库车（停车方向），班计划增加“重联”备注，综控室增加两日客调对比。";
+        string build = "build 39 - v180727";
+        string readMe = "build39更新内容:\n" +
+            "现在可以判断旅客列车有无出库车（停车方向），班计划增加“重联”备注，综控室增加两日客调对比。修复Bugs";
 
         public Main()
         {
@@ -2018,7 +2018,10 @@ namespace TimeTableAutoCompleteTool
                             _ds.extraText = dailyScheduleModel[i].extraText.Trim();
                         if (dailyScheduleModel[i].tipsText != null)
                             _ds.tipsText = dailyScheduleModel[i].tipsText.Trim();
-
+                        if (commandModel[j].trainNumber.Equals("G368"))
+                        {
+                            int isa = 0;
+                        }
                         //车型
                         switch (commandModel[j].trainConnectType)
                         {
@@ -2029,6 +2032,10 @@ namespace TimeTableAutoCompleteTool
                                         _ds.hasDifferentPart = true;
                                     }
                                 _ds.trainConnectType = "8";
+                                if(_ds.extraText == null)
+                                {
+                                    _ds.extraText = "";
+                                }
                                 if (_ds.extraText != null)
                                 {
                                     //判断南北停靠
@@ -2040,10 +2047,6 @@ namespace TimeTableAutoCompleteTool
                                             //特殊情况-出库车，寻找客调令中有没有该车次的出库
                                             //找同一组车底内，有无0G/D(XXX-1)或者0G/D(XXX+1)或者-3+3，有的话是出库车，则停南
                                             string thisTrainNumber = _ds.trainNumber.Split('/')[0];
-                                            if (thisTrainNumber.Contains("1892"))
-                                            {
-                                                int l = 0;
-                                            }
                                             string trainType = "";
                                             string splitedNumber = "";
                                             string[] targetTrainNum = new string[4];
@@ -2092,7 +2095,7 @@ namespace TimeTableAutoCompleteTool
                                                         }
                                                     }
                                                     foreach (CommandModel _cm in commandModel)
-                                                    {
+                                                    {//车可能是0J
                                                         if (_cm.trainNumber.Contains("0" + trainType) &&
                                                             _cm.trainId.Equals(commandModel[j].trainId))
                                                         {
@@ -2125,16 +2128,17 @@ namespace TimeTableAutoCompleteTool
                                                     }
                                                 }
                                             }
-                                            else if (_ds.upOrDown == 1)
-                                            {
-                                                string extraText = _ds.extraText + " （南）";
-                                                _ds.extraText = extraText;
-                                            }
-                                            else
-                                            {
-                                                string extraText = _ds.extraText + " （南/北停靠无法识别）";
-                                                _ds.extraText = extraText;
-                                            }
+
+                                        }
+                                        else if (_ds.upOrDown == 1)
+                                        {
+                                            string extraText = _ds.extraText + " （南）";
+                                            _ds.extraText = extraText;
+                                        }
+                                        else
+                                        {
+                                            string extraText = _ds.extraText + " （南/北停靠无法识别）";
+                                            _ds.extraText = extraText;
                                         }
                                         if (_ds.extraText.Contains("重联"))
                                         {
@@ -2172,6 +2176,10 @@ namespace TimeTableAutoCompleteTool
                                     {
                                         _ds.hasDifferentPart = true;
                                     }
+                                if(_ds.extraText == null)
+                                {
+                                    _ds.extraText = "";
+                                }
                                 if (_ds.extraText != null)
                                 {
                                     if (_ds.extraText.Contains("南") ||
